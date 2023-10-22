0c311b74c04d4978a718e17999cdc29ecba7efe7
mrhorrocks <mr.horrocks@gmail.com>
2023-10-22 20:58:47 +0100

    head config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    head: {
+        htmlAttrs: {lang: 'en'},
+        charset: 'utf-8',
+        viewport: 'width=device-width, initial-scale=1',
+        title: 'Car Dealer Component',
+        meta: [{ name: 'description', content: 'Car Dealer Component' }],
+        // link: [
+        //     { 
+        //         rel: 'stylesheet', 
+        //         href: 'https://fonts.googleapis.com/css2?family=Lobster&family=Yanone+Kaffeesatz:wght@300;400;500;600;700&display=swap',
+        //     }
+        // ],
+    }
+},
+  devtools: { enabled: false }
 })
