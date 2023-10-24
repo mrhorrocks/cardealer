fc91183a44bad3642095c83244e35d7a45c3c0fb
mrhorrocks <mr.horrocks@gmail.com>
2023-10-24 22:18:20 +0100

    scss nesting refactored

--- components/CarCard.vue
+++ components/CarCard.vue
@@ -42,7 +42,6 @@
     </article>
 </template>
 
-
 <style lang="scss" scoped>
 .car-card {
     position: relative;
@@ -54,92 +53,78 @@
     border-radius: 0rem;
     overflow: hidden;
     background-color: #ffffff;
-}
-
-.image-container {
-    position: relative;
-    width: -moz-max-content;
-    width: 100%;
-    overflow-x: auto;
-    white-space: nowrap;
-
-    img {
-        width: 113px;
-        border-radius: 1rem;
-        margin-right: 0.5rem;
-    }
-
-}
-
-.listing-tag {
-    position: absolute;
-    top: 96px;
-    right: 35px;
-
-    span {
-        background-color: #3F3A50;
-        font-size: 0.8rem;
-        line-height: 27px;
-        padding: 2px 10px;
-        margin: 0 5px 0 5px;
-        border: 1px solid rgb(255, 255, 255, 0.25);
-        color: #ffffff;
-        border-radius: 8px;
-    }
-}
-
-p {
-    margin: 0 0 0.25rem 0;
-}
-
-.car-details {
-    position: relative;
-    padding: 0.75rem 0.5rem 0.5rem 0.5rem;
-    font-size: 0.75rem;
-
-    span {
-        font-size: 1rem;
-        font-weight: 600;
-    }
-
-    .favorite {
+    .listing-tag {
         position: absolute;
-        top: 10px;
-        right: 10px;
-        height: 20px;
-        width: 20px;
-    }
-
-    .spec-tags {
-        margin-top: 1rem;
-        width: 50%;
-        float: left;
-
+        top: 96px;
+        right: 35px;
         span {
-            display: inline-block;
-            font-size: 12px;
-            line-height: 13px;
-            padding: 0px 5px 5px 0px;
-            margin: 0 3px 3px 0px;
-            color: #55595D;
-            border-radius: 0px;
-            border-right: 1px solid rgb(85, 89, 93, 0.25);
-
-            &:last-child {
-                border: 0;
-            }
+            background-color: #3F3A50;
+            font-size: 0.8rem;
+            line-height: 27px;
+            padding: 2px 10px;
+            margin: 0 5px 0 5px;
+            border: 1px solid rgb(255, 255, 255, 0.25);
+            color: #ffffff;
+            border-radius: 8px;
         }
     }
-
-    .pricing {
-        margin-top: 1rem;
-        width: 50%;
-        float: right;
-        text-align: right;
-
-        .total-price a {
-            color: #7572FF;
-            margin-left: 5px;
+    .image-container {
+        position: relative;
+        width: -moz-max-content;
+        width: 100%;
+        overflow-x: auto;
+        white-space: nowrap;
+        img {
+            width: 113px;
+            border-radius: 1rem;
+            margin-right: 0.5rem;
+        }
+    }
+    .car-details {
+        position: relative;
+        padding: 0.75rem 0.5rem 0.5rem 0.5rem;
+        font-size: 0.75rem;
+        p {
+            margin: 0 0 0.25rem 0;
+        }
+        .favorite {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            height: 20px;
+            width: 20px;
+        }
+        span {
+            font-size: 1rem;
+            font-weight: 600;
+        }
+        .spec-tags {
+            margin-top: 1rem;
+            width: 50%;
+            float: left;
+            span {
+                display: inline-block;
+                font-size: 12px;
+                line-height: 13px;
+                padding: 0px 5px 5px 0px;
+                margin: 0 3px 3px 0px;
+                color: #55595D;
+                border-radius: 0px;
+                border-right: 1px solid rgb(85, 89, 93, 0.25);
+                &:last-child {
+                    border: 0;
+                }
+            }
+        }
+        .pricing {
+            margin-top: 1rem;
+            width: 50%;
+            float: right;
+            text-align: right;
+            .total-price a {
+                color: #7572FF;
+                margin-left: 5px;
+            }
         }
     }
 }
@@ -151,67 +136,58 @@ p {
         margin: 12px;
         min-width: inherit;
         box-shadow: 0px 6px 20px #00000040;
-    }
-
-    .listing-tag {
-        position: absolute;
-        top: 10px;
-        left: 10px;
-        z-index: 100;
-
-        span {
-            background-color: #3F3A50;
-            font-size: 1rem;
-            line-height: 27px;
-            padding: 2px 10px;
-            margin: 0 5px 0 5px;
-            border: 1px solid rgb(255, 255, 255, 0.25);
-            color: #ffffff;
-            border-radius: 8px;
-        }
-    }
-
-    .image-container {
-        position: relative;
-        width: max-content;
-
-        img {
-            width: 257px;
-            border-radius: 0;
-        }
-
-    }
-
-    .car-details {
-        .spec-tags {
+        .listing-tag {
             position: absolute;
-            top: -33px;
-            left: 0;
-            width: 100%;
-            margin: 0;
-
+            top: 10px;
+            left: 10px;
+            z-index: 100;
             span {
-                display: inline-block;
                 background-color: #3F3A50;
-                font-size: 0.625rem;
-                line-height: 18px;
+                font-size: 1rem;
+                line-height: 27px;
                 padding: 2px 10px;
-                margin: 0 3px 3px 5px;
-                border: 1px solid rgba(255, 255, 255, 0.25);
+                margin: 0 5px 0 5px;
+                border: 1px solid rgb(255, 255, 255, 0.25);
                 color: #ffffff;
                 border-radius: 8px;
             }
         }
-
-        .pricing {
-            margin-top: 1rem;
-            width: 50%;
-            float: left;
-            text-align: left;
-
-            .total-price a {
-                color: #7572FF;
-                margin-left: 5px;
+        .image-container {
+            position: relative;
+            width: max-content;
+            img {
+                width: 257px;
+                border-radius: 0;
+            }
+        }
+        .car-details {
+            .spec-tags {
+                position: absolute;
+                top: -33px;
+                left: 0;
+                width: 100%;
+                margin: 0;
+                span {
+                    display: inline-block;
+                    background-color: #3F3A50;
+                    font-size: 0.625rem;
+                    line-height: 18px;
+                    padding: 2px 10px;
+                    margin: 0 3px 3px 5px;
+                    border: 1px solid rgba(255, 255, 255, 0.25);
+                    color: #ffffff;
+                    border-radius: 8px;
+                }
+            }
+            .pricing {
+                margin-top: 1rem;
+                width: 50%;
+                float: left;
+                text-align: left;
+                .total-price a {
+                    color: #7572FF;
+                    margin-left: 5px;
+                }
             }
         }
     }
@@ -220,35 +196,33 @@ p {
 @media only screen and (min-width: 1024px) {
     .car-card {
         max-width: 350px;
-    }
-
-    .image-container {
-        img {
-            width: 350px;
+        .image-container {
+            img {
+                width: 350px;
+            }
         }
-
     }
 }
 
 @media only screen and (min-width: 1280px) {
     .car-card {
         max-width: 290px;
-    }
-    .image-container {
-        img {
-            width: 290px;
-        }
+        .image-container {
+            img {
+                width: 290px;
+            }
 
+        }
     }
 }
 
 @media only screen and (min-width: 1440px) {
     .car-card {
         max-width: 333px;
-    }
-    .image-container {
-        img {
-            width: 333px;
+        .image-container {
+            img {
+                width: 333px;
+            }
         }
     }
 }
