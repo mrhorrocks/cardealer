99bcbae6ec7ebc63d0e9c4b84585eb50a234e6f9
mrhorrocks <mr.horrocks@gmail.com>
2023-11-16 13:39:21 +0000

    Favourite star update

--- components/FavouriteStar.vue
+++ components/FavouriteStar.vue
@@ -2,11 +2,11 @@
     <div @click.stop.prevent="toggleFavorite">
         <i :class="['fa', { 'fa-star': isFavorite, 'fa-star-o': !isFavorite }]"></i>
         
-        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor"
             stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star">
             <polygon
                 points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-        </svg>
+        </svg> -->
 
     </div>
 </template>
